236f90071f82ac9742ee83527533a3ca58da178c
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-27 09:43:15 -0800

    pass Pizza() object test

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-.floating {
-    position: absolute;
-    bottom: 100px;
-    right: 100px;
+// Business Logic
+
+function Pizza(size, toppings) {
+    this.size = size;
+    this.toppings = toppings;
+}
+
+Pizza.prototype.pizzaCost = function () {
+    let pizzaCost = 0;
+    if (this.size === "Small") {
+        pizzaCost += 15;
+    }
+    else if (this.size = "Medium") {
+        pizzaCost += 20;
+    }
+    else if (this.size = "Large") {
+        pizzaCost += 20;
+    }
 }
